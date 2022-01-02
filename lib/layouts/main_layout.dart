c5d7d4e1c602c3f142e6041e0d17a336f3cd249c
piousck <pious.james007@gmail.com>
2022-01-02 13:33:11 +0000

    commit

---
+++ lib/layouts/main_layout.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:roster_web_app/pages/home_page.dart';
+
+class MainLayout
+    extends StatelessWidget {
+  const MainLayout({Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //Top Border
+        topBorder(),
+        mainContentWindow(),
+      ],
+    );
+  }
+
+//Main window on the right of sidebar for content display
+  Expanded mainContentWindow() {
+    return const Expanded(
+      flex: 100,
+      child: MyHomePage(),
+    );
+  }
+
+//Top border used as a decoration
+  Expanded topBorder() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.teal,
+      ),
+    );
+  }
+}
